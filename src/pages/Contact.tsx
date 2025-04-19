85e8ff35761c3f66913a15c4042340b34d2fce76
Murt4z4 <murtaza.akbar@gmail.com>
2025-04-19 15:00:56 +0500

    cal update and typography strcuture

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -13,13 +13,13 @@ const Contact = () => {
     script.type = 'text/javascript';
     script.innerHTML = `
       (function (C, A, L) { let p = function (a, ar) { a.q.push(ar); }; let d = C.document; C.Cal = C.Cal || function () { let cal = C.Cal; let ar = arguments; if (!cal.loaded) { cal.ns = {}; cal.q = cal.q || []; d.head.appendChild(d.createElement("script")).src = A; cal.loaded = true; } if (ar[0] === L) { const api = function () { p(api, arguments); }; const namespace = ar[1]; api.q = api.q || []; if(typeof namespace === "string"){cal.ns[namespace] = cal.ns[namespace] || api;p(cal.ns[namespace], ar);p(cal, ["initNamespace", namespace]);} else p(cal, ar); return;} p(cal, ar); }; })(window, "https://app.cal.com/embed/embed.js", "init");
-      Cal("init", "15min", {origin:"https://cal.com"});
-      Cal.ns["15min"]("inline", {
+      Cal("init", "discovery", {origin:"https://cal.com"});
+      Cal.ns.discovery("inline", {
         elementOrSelector:"#my-cal-inline",
         config: {"layout":"month_view"},
-        calLink: "murtaza-akbar/15min",
+        calLink: "murtaza-akbar/discovery",
       });
-      Cal.ns["15min"]("ui", {"hideEventTypeDetails":false,"layout":"month_view"});
+      Cal.ns.discovery("ui", {"hideEventTypeDetails":false,"layout":"month_view"});
     `;
     document.body.appendChild(script);
 
